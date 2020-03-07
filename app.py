e2a4a227f33c8b909230f36da81956c1918e50b8
JF1HNL <kimossan88@gmail.com>
2020-03-07 21:25:21 +0900

    枠組み作成

--- app.py
+++ app.py
@@ -1,2 +1,29 @@
-import token
-print(token.token)
+# token情報を別のファイルにおく
+import token 
+# インストールした discord.py を読み込む
+import discord
+
+# 自分のBotのアクセストークンに置き換えてください
+TOKEN = token.token
+
+# 接続に必要なオブジェクトを生成
+client = discord.Client()
+
+# 起動時に動作する処理
+@client.event
+async def on_ready():
+  # 起動したらターミナルにログイン通知が表示される
+  print('...ready')
+
+# メッセージ受信時に動作する処理
+@client.event
+async def on_message(message):
+  # メッセージ送信者がBotだった場合は無視する
+  if message.author.bot:
+    return
+  # 「/neko」と発言したら「にゃーん」が返る処理
+  if message.content == '/neko':
+    await message.channel.send('にゃーん')
+
+# Botの起動とDiscordサーバーへの接続
+client.run(TOKEN)
