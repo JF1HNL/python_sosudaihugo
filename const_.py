5089001b49c87cc1fe70c465ba8b7c5e4987325b
JF1HNL <kimossan88@gmail.com>
2020-03-17 20:25:51 +0900

    const.pyのダミー追加

---
+++ const_.py
@@ -0,0 +1,32 @@
+token = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+role = {
+  'kankyaku': {
+    'id': 000000000000000000,
+    'ja': '観客'
+  },
+  'player-a-1': {
+    'id':000000000000000000,
+    'ja':'プレイヤーA1'
+  },
+  'player-a-2':{
+    'id': 000000000000000000,
+    'ja':'プレイヤーA2'
+  },
+  'player-b-1':{
+    'id':000000000000000000,
+    'ja':'プレイヤーB1'
+  },
+  'player-b-2':{
+    'id':000000000000000000,
+    'ja':'プレイヤーB2'
+  }
+}
+channel_id = {
+  'bot_control' : 000000000000000000,
+  'player-a-1': 000000000000000000,
+  'player-a-2': 000000000000000000,
+  'jikkyo-a': 000000000000000000,
+  'player-b-1': 000000000000000000,
+  'player-b-2': 000000000000000000,
+  'jikkyo-b': 000000000000000000
+}
